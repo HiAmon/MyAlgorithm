3a3b3ea2de581a1bbac0b442d2dede1fa585a3c1
HiAmon <amon_winni@outlook.com>
2022-05-25 19:35:33 +0800

    commit for traverse/construct tree 核心：中序遍历分割左右子树 分治法->递归 1. 退出条件 2. 入参出参 3. 单层逻辑

--- nowcode/src/main/java/topic/dfs/P105_ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ nowcode/src/main/java/topic/dfs/P105_ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -38,33 +38,102 @@ package topic.dfs;
 
 import topic.TreeNode;
 
+import java.util.HashMap;
+
 public class P105_ConstructBinaryTreeFromPreorderAndInorderTraversal{
     public static void main(String[] args) {
         Solution solution = new P105_ConstructBinaryTreeFromPreorderAndInorderTraversal().new Solution();
-        
+        int[] preorder = new int[]{3,9,20,15,7};
+        int[] inorder = new int[]{9,3,15,20,7};
+        TreeNode head = solution.buildTree(preorder, inorder);
     }
 
-//leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return null;
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        int[] preorder;
+        int[] inorder;
+        HashMap<Integer,Integer> inMap = new HashMap<>();//inMap作为参照就够了，直接对preOrder遍历，不需要preMap
+
+        public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+            int len = preorder.length;
+            for (int i = 0; i < len; i++) {
+                inMap.put(inorder[i],i);
+            }
+
+            this.preorder = preorder;
+            this.inorder = inorder;
+            return traverse(0, len - 1, 0, len - 1);
+        }
+
+        /**
+         * 递归 -> 深度优先遍历
+         * @param preLeft 当前处理的左区间 preorder中对应下标
+         * @param preRight 当前处理的右区间
+         * @param inLeft 当前处理的左区间 inorder中中对应下标
+         * @param inRight 当前处理的右区间
+         * @return
+         */
+        public TreeNode traverse(int preLeft, int preRight, int inLeft, int inRight ){
+//            //当前区间只有一个元素
+//            if (inRight-inLeft == 1){
+//                return new TreeNode(inorder[inLeft]);
+//            }
+//
+//            //走到叶子节点
+//            if (inRight-inLeft < 1){
+//                return null;
+//            }
+//
+//            TreeNode curParent = new TreeNode(preorder[preLeft]);
+//
+//            int curIn = inMap.get(curParent.val);
+//
+//            curParent.left = traverse(
+//                    inLeft,//0
+//                    curIn,//1
+//                    preLeft,//0
+//                    preLeft + (curIn - inLeft)//0+1-0=1
+//            );
+//
+//            curParent.right = traverse( curIn + 1,
+//                    inRight,
+//                     preLeft + (curIn - inLeft),
+//                    preRight - 1
+//            );
+//            return curParent;
+
+
+
+            if (inLeft > inRight || preLeft > preRight) return null;
+
+            // val 为前序遍历第一个的值，也即是根节点的值
+            // idx 为根据根节点的值来找中序遍历的下标
+            int idx = inLeft, val = preorder[preLeft];
+            TreeNode curParent = new TreeNode(val);
+            for (int i = inLeft; i <= inRight; i++) {
+                if (inorder[i] == val) {
+                    idx = i;
+                    break;
+                }
+            }
+
+            // 根据 idx 来递归找左右子树
+            curParent.left = traverse(
+                    preLeft + 1,
+                    preLeft + (idx - inLeft),
+                     inLeft,
+                    idx - 1);
+            curParent.right = traverse(
+                    preLeft + (idx - inLeft) + 1,
+                    preRight,
+                     idx + 1,
+                    inRight);
+            return curParent;
+        }
+
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
+    //leetcode submit region end(Prohibit modification and deletion)
 
 }
